81eefa13d1d616abf7badb34c157e8a827c7dda9
Ebi-Tech <difebi14@gmail.com>
2023-09-18 16:25:03 +0100

    Fixed the screen cuts

--- app/dist/page.js
+++ app/dist/page.js
@@ -3,7 +3,7 @@ exports.__esModule = true;
 var LeftSide_1 = require("@/component/LeftSide");
 var RightSide_1 = require("@/component/RightSide");
 function Home() {
-    return (React.createElement("main", { className: "flex bg-White w-50 h-50 items-center justify-center p-24" },
+    return (React.createElement("main", { className: "flex bg-White w-screen h-screen items-center justify-center p-24" },
         React.createElement("div", { className: 'flex w-[45%] h-[85%] bg-White shadow-2xl rounded-xl overflow-hidden ' },
             React.createElement(LeftSide_1["default"], null),
             React.createElement(RightSide_1["default"], null))));
--- component/RightSide.tsx
+++ component/RightSide.tsx
@@ -6,42 +6,41 @@ import visual from "@/public/assets/images/icon-visual.svg"
 
 const RightSide = () => {
   return (
-    <div className='w-[50%] h=full p-5 flex flex-col '>
+    <div className='w-[50%] h=full p-5 flex flex-col gap-4 '>
         <h2>Summary</h2>
         <div className='flex flex-col   '>
-<br />
-            <button className='flex flex-row w-full p-4 rounded-lg font-semibold items-centre
+
+            <button className='flex flex-row w-full p-1 rounded-lg font-semibold items-centre
              bg-Light-red bg-opacity-5'>
                 <Image src={reaction} alt='react' width={20} priority className='mr-4'/>
                 <p className='text-[16px] text-Light-red mr-auto'>Reaction</p>
                 <p className='text-[14px] '>80  <span className='text-Light-lavender'> / 100</span></p>
             </button>
 <br />
-            <button className='flex flex-row w-full  p-4 rounded-lg font-semibold items-center
+            <button className='flex flex-row w-full  p-1 rounded-lg font-semibold items-center
              bg-Orangey-yellow bg-opacity-5'>
                 <Image src={memory} alt='react' width={20} priority className='mr-4'/>
                 <p className='text-[16px] text-Orangey-yellow mr-auto'>Memory</p>
                 <p className='text-[14px] '>92  <span className='text-Light-lavender'>  /  100</span></p>
             </button>
 <br />
-            <button className='flex flex-row w-full  p-4 rounded-lg font-semibold items-center
+            <button className='flex flex-row w-full  p-1 rounded-lg font-semibold items-center
              bg-Green-teal bg-opacity-5'>
                 <Image src={verbal} alt='react' width={20} priority className='mr-4'/>
                 <p className='text-[16px] text-Green-teal mr-auto'>Verbal</p>
                 <p className='text-[14px] '>61  <span className='text-Light-lavender'>  /  100</span></p>
             </button>
 <br />
-            <button className='flex flex-row w-full  p-4 rounded-lg font-semibold items-center
+            <button className='flex flex-row w-full  p-1 rounded-lg font-semibold items-center
              bg-Cobalt-blue bg-opacity-5'>
                 <Image src={visual} alt='react' width={20} priority className='mr-4'/>
                 <p className='text-[16px] text-Cobalt-blue mr-auto'>Visual</p>
                 <p className='text-[14px] '>72  <span className='text-Light-lavender'>  /  100</span></p>
             </button>
-<br />
         </div>
         <button className='flex flex-row  rounded-full items-center justify-center 
-           w-full h-1 p-6 bg-gradient-to-b from-Violet-blue to-Persian-blue '>
-            <p className='text-White text-[17px] font-semibold '>Continue</p>
+           w-full h-1 p-5 bg-gradient-to-b from-Violet-blue to-Persian-blue '>
+            <p className='text-White text-[14px] font-semibold '>Continue</p>
   </button>
     </div>
 
