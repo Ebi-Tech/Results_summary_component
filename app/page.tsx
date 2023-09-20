be31aaabbea8acbcdf7acfd3716158cbda21d61b
Ebi-Tech <difebi14@gmail.com>
2023-09-20 09:02:01 +0100

    Fixed pages.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
-import LeftSide from '@/component/LeftSide'
-import RightSide from '@/component/RightSide'
+"use client";
 
-import Image from 'next/image'
+import LeftSide from "@/component/LeftSide";
+import RightSide from "@/component/RightSide";
 
 export default function Home() {
   return (
-    <main className="flex bg-White w-screen h-screen items-center justify-center p-24">
-
-      <div className='flex w-[45%] h-[85%] bg-White shadow-2xl rounded-xl overflow-hidden '>
-        
-        <LeftSide/>
-
-        <RightSide/>
-
+    <div
+      className="w-screen h-screen bg-White flex items-center justify-center p-4
+    mobile:p-0
+    mobileBig:p-0
+    "
+    >
+      <div
+        className="w-[45%] h-[70%] bg-Off-White shadow-2xl shadow-Green-teal/20 rounded-3xl overflow-hidden text-Dark-gray-blue flex
+      mobile:flex-col mobile:w-full mobile:h-full mobile:rounded-none
+      mobileBig:flex-col mobileBig:w-full mobileBig:h-full mobileBig:rounded-none
+      "
+      >
+        <LeftSide />
+        <RightSide />
       </div>
-    </main>
-  )
+    </div>
+  );
 }
