fdca331a5c6c0719c1760dbcb808c9c76e423bca
Divine Ebube Ifechukwude <difebi14@gmail.com>
2023-09-20 18:14:36 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import RightSide from '@/component/RightSide'
 
 export default function Home() {
   return (
-    <main className="flex bg-White w-screen h-screen items-center justify-center p-24">
+    <main className="w-screen h-screen bg-White flex items-center justify-center p-4
+    mobile:p-0
+    mobileBig:p-0">
 
-      <div className='flex w-[45%] h-[85%] bg-White shadow-2xl rounded-xl overflow-hidden '>
+      <div className='w-[45%] h-[70%] bg-White shadow-2xl shadow-Green-teal/20 rounded-3xl overflow-hidden text-Dark-gray-blue flex
+      mobile:flex-col mobile:w-full mobile:h-full mobile:rounded-none
+      mobileBig:flex-col mobileBig:w-full mobileBig:h-full mobileBig:rounded-none'>
         
         <LeftSide/>
 
